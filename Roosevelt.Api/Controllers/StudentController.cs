d25ed07d38371ef1a5c26f73388daa24a74034b8
jmacfarland <macfarland.jamison@gmail.com>
2018-11-17 11:35:54 -0500

    Added CSV file input to inMemory DB

--- Roosevelt.Api/Controllers/StudentController.cs
+++ Roosevelt.Api/Controllers/StudentController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using Roosevelt.Api.Models;
+using System.IO;
+using System;
+using LumenWorks.Framework.IO.Csv;
 
 namespace Roosevelt.Api.Controllers
 {
@@ -17,9 +20,26 @@ namespace Roosevelt.Api.Controllers
 
             if (_context.Students.Count() == 0)
             {
-                // Populate database
-                _context.Students.Add(new Student { first_name = "John" });
+                Console.WriteLine("Starting import CSV!");
+                using (CsvReader csv = new CsvReader(new StreamReader("student_data.csv"), true))
+                {
+                    while(csv.ReadNextRecord())
+                    {
+                        _context.Students.Add(new Student {
+                            Id=csv[0],
+                            first_name=csv[1],
+                            last_name=csv[2],
+                            age=csv[3],
+                            parent_1=csv[4],
+                            parent_2=csv[5],
+                            phone=csv[6],
+                            attendance=csv[7],
+                            days_membership=csv[8]
+                        });
+                    }
+                }
                 _context.SaveChanges();
+                Console.WriteLine("Finished importing CSV!");
             }
         }
 
--- Roosevelt.Api/Models/Student.cs
+++ Roosevelt.Api/Models/Student.cs
@@ -6,16 +6,16 @@ namespace Roosevelt.Api.Models
     public class Student
     {
         [Key]
-        public long Id { get; set; }
+        public string Id { get; set; }
         [Required]
         public string first_name { get; set; }
         [Required]
         public string last_name { get; set; }
-        public int age { get; set; }
+        public string age { get; set; }
         public string parent_1 { get; set; }
         public string parent_2 { get; set; }
         public string phone { get; set; }
-        public int attendance { get; set; }
-        public int days_membership { get; set; }
+        public string attendance { get; set; }
+        public string days_membership { get; set; }
     }
 }
